140cba07f25abe056c07e916a8336a727487a522
Sergej Schmal <serg.schmal@gmx.de>
2019-03-07 05:46:17 +0100

    Add project files.

---
+++ WiredBrainCoffe.CustomerApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WiredBrainCoffe.CustomerApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Customer added!");
+            await messageDialog.ShowAsync();
+        }
+    }
+}
