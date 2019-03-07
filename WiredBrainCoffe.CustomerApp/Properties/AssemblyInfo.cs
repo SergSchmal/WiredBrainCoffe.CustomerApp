3affb482a407813295ec39d0db16c71096e9b665
Sergej Schmal <serg.schmal@gmx.de>
2019-03-07 06:59:30 +0100

    Rename of the solution

--- WiredBrainCoffe.CustomerApp/Properties/AssemblyInfo.cs
+++ WiredBrainCoffe.CustomerApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WiredBrainCoffe.CustomerApp")]
+[assembly: AssemblyTitle("WiredBrainCoffee.CustomerApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WiredBrainCoffe.CustomerApp")]
+[assembly: AssemblyProduct("WiredBrainCoffee.CustomerApp")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
